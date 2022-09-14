8cdbfbafeeb1692d8ce618ad38d3317299164021
Jbachtiger <jbachtiger@gmail.com>
2022-09-14 14:26:54 +0000

    Fix issue with dropdown not displaying by editing package.json files

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-jb.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.baseURL = "https://drf-api-jb.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+
+
+
+
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
+import axios from "axios";
 
+import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
+
 import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
-  const setCurrentUser =useSetCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
   const [signInData, setSignInData] = useState({
     username: "",
@@ -27,16 +28,9 @@ function SignInForm() {
   const [errors, setErrors] = useState({});
 
   const history = useHistory();
-
-  const handleChange = (event) => {
-    setSignInData({
-      ...signInData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
+
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
@@ -46,25 +40,32 @@ function SignInForm() {
     }
   };
 
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
-
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
-                className={styles.Input}
                 type="text"
                 placeholder="Username"
                 name="username"
+                className={styles.Input}
+                value={username}
                 onChange={handleChange}
               />
             </Form.Group>
             {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
+              <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
@@ -72,27 +73,27 @@ function SignInForm() {
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
-                className={styles.Input}
                 type="password"
                 placeholder="Password"
                 name="password"
+                className={styles.Input}
+                value={password}
                 onChange={handleChange}
               />
             </Form.Group>
             {errors.password?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
+              <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
-
             <Button
-              type="submit"
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
             >
               Sign in
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
-              <Alert variant="warning" className="mt-3" key={idx}>
+              <Alert key={idx} variant="warning" className="mt-3">
                 {message}
               </Alert>
             ))}
